f81635ed83af9fe6ee3e4c58ed4eee157b069514
João Felipe P <jf.pelliccione@gmail.com>
2021-09-23 19:11:18 -0300

    Adicionando searchbox, para input do usuário.

--- projetos-trybe/1-Fundamentos-Desenvolvimento-Web/09_shopping-cart-project/script.js
+++ projetos-trybe/1-Fundamentos-Desenvolvimento-Web/09_shopping-cart-project/script.js
@@ -32,27 +32,27 @@ function createProductItemElement({ id, title, thumbnail }) { // Alterei o nome 
   return section;
 } // Função definida pela Trybe.
 
-function getComputerPrice(htmlElement) { // [REQUISITO 5]: Função que capta o preço do computador adicionado ou removido do carrinho de compras.
-  const compInfoStringForm = htmlElement.innerText; // 'SKU: MLB1986960528 | NAME: Notebook Multilaser Legacy Cloud Pc131 Cinza 14 , Intel Atom X5-z8350 2gb De Ram 32gb Ssd 1366x768px Windows 10 Home | PRICE: $1633.91'
-  const compInfoArrayForm = compInfoStringForm.split('|'); // ['SKU: MLB1986960528 ', ' NAME: Notebook Multilaser Legacy Cloud Pc131 Cinz…0 2gb De Ram 32gb Ssd 1366x768px Windows 10 Home ', ' PRICE: $1633.91']
-  const compPriceStringForm = compInfoArrayForm[compInfoArrayForm.length - 1]; // 'PRICE: $1633.91'
-  const compPriceArrayForm = compPriceStringForm.split('$'); // [' PRICE: ', '1633.91']
-  const compPriceString = compPriceArrayForm[compPriceArrayForm.length - 1]; // '1633.91'
-  const compPrice = Number(Number(compPriceString).toFixed(2)); // 1633.91
-  return compPrice;
+function getProductPrice(htmlElement) { // [REQUISITO 5]: Função que capta o preço do produto adicionado ou removido do carrinho de compras.
+  const prodInfoStringForm = htmlElement.innerText; // 'SKU: MLB1986960528 | NAME: Notebook Multilaser Legacy Cloud Pc131 Cinza 14 , Intel Atom X5-z8350 2gb De Ram 32gb Ssd 1366x768px Windows 10 Home | PRICE: $1633.91'
+  const prodInfoArrayForm = prodInfoStringForm.split('|'); // ['SKU: MLB1986960528 ', ' NAME: Notebook Multilaser Legacy Cloud Pc131 Cinz…0 2gb De Ram 32gb Ssd 1366x768px Windows 10 Home ', ' PRICE: $1633.91']
+  const prodPriceStringForm = prodInfoArrayForm[prodInfoArrayForm.length - 1]; // 'PRICE: $1633.91'
+  const prodPriceArrayForm = prodPriceStringForm.split('$'); // [' PRICE: ', '1633.91']
+  const prodPriceString = prodPriceArrayForm[prodPriceArrayForm.length - 1]; // '1633.91'
+  const productPrice = Number(Number(prodPriceString).toFixed(2)); // 1633.91
+  return productPrice;
 }
 
-function saveShopCart() { // [REQUISITO 4]: Função que armazena o atual estado do carrinho de compras no localStorage sempre que um computador for adicionado [addOnShopCart(] ou removido [cartItemClickListener()].
+function saveShopCart() { // [REQUISITO 4]: Função que armazena o atual estado do carrinho de compras no localStorage sempre que um produto for adicionado [addOnShopCart(] ou removido [cartItemClickListener()].
   const shopCartContent = getShopCartContainer().innerHTML; // Capturando todo o conteúdo dentro da <ol class="cart__items"></ol>.
   localStorage.setItem('userShopCart', shopCartContent); // Salvando o conteúdo captado acima, no localStorage.
   const cartTotalValue = cartValueContainer().innerHTML; // Capturando todo o conteúdo dentro de <output class="total-price"></output>.
   localStorage.setItem('cartTotalValue', cartTotalValue); // Salvando o conteúdo captado acima, no localStorage.
 }
 
-function cartItemClickListener(event) { // [REQUISITO 3]: Função que permite que os computadores adicionados ao carrinho de compras, sejam removidos quando clicados.
+function cartItemClickListener(event) { // [REQUISITO 3]: Função que permite que os produtos adicionados ao carrinho de compras, sejam removidos quando clicados.
   const cartTotalCont = cartValueContainer(); // Captando o <output class="total-price"></output> e adicionando-o à constante cartTotalCont.
   event.target.remove(); // Removendo o item do carrinho que for clicado.
-  const res = Number((Number(cartTotalCont.innerHTML) - getComputerPrice(event.target)).toFixed(2)); // Captando o preço do computador recém-excluído do carrinho de compras e o subtraindo do innerHTML de <output class="total-price"></output>.
+  const res = Number((Number(cartTotalCont.innerHTML) - getProductPrice(event.target)).toFixed(2)); // Captando o preço do produto recém-excluído do carrinho de compras e o subtraindo do innerHTML de <output class="total-price"></output>.
   cartTotalCont.innerHTML = res; // Imputando o resultado da linha acima dentro de <output class="total-price"></output>.
   
   saveShopCart();
@@ -62,9 +62,9 @@ function getShopCartFromLocalStorage() { // [REQUISITO 4]: Função que capta, d
   getShopCartContainer().innerHTML = localStorage.getItem('userShopCart');
   cartValueContainer().innerHTML = localStorage.getItem('cartTotalValue');
 
-  // Todos os elementos captados do localStorage devem poder ser removidos quando clicados, assim como ocorre quando um novo computador é adicionado.
-  Array.from(getShopCartContainer().children).forEach((computer) => {
-    computer.addEventListener('click', cartItemClickListener);
+  // Todos os elementos captados do localStorage devem poder ser removidos quando clicados, assim como ocorre quando um novo produto é adicionado.
+  Array.from(getShopCartContainer().children).forEach((product) => {
+    product.addEventListener('click', cartItemClickListener);
   });
 }
 
@@ -76,55 +76,75 @@ function createCartItemElement({ id, title, price }) { // [REQUISITO 2]: Alterei
   return li;
 }
 
-/* [REQUISITO 1]: Função que verifica se o argumento imputado em 'url' é igual à "https://api.mercadolibre.com/sites/MLB/search?q=computador".
-  - Caso seja: A função retornará um array de objetos onde, cada objeto, equivale à um computador diferente.
-  - Caso não seja: A função lançará um erro pedindo para que se verifique a URL imputada em 'url'.
+/* [REQUISITO 1]: Função que faz um fetch para uma 'url' similar à "https://api.mercadolibre.com/sites/MLB/search?q=produto-qualquer".
+  - Caso o fetch corra bem: A função retornará um array de objetos onde, cada objeto, equivale à um produto.
+  - Caso o fetch corra mal: A função lançará um erro pedindo para que o usuário pesquise outro produto.
   */
-async function getMeliComputers(url) {
-  if (url === 'https://api.mercadolibre.com/sites/MLB/search?q=computador') {
-    return fetch(url)
-      .then((response) => response.json()) // Alterando a response da requisição para o formato JSON.
-      .then((allList) => allList.results); // Retornando um array de objetos onde, cada objeto, equivale à um computador diferente.
-      // REMOVE LOADING
-  }
-  throw new Error('URL não bate com https://api.mercadolibre.com/sites/MLB/search?q=computador');
+async function getMeliProducts(url) {
+  return fetch(url)
+    .then((response) => response.json().then((allList) =>
+      response.ok ? Promise.resolve(allList.results) : Promise.reject(new Error('Erro: Pesquise outro item!'))
+    ));
+/*
+  .then((response) => response.json()) // Alterando a response da requisição para o formato JSON.
+  .then((allList) => allList.results); // Retornando um array de objetos onde, cada objeto, equivale à um produto.
+*/
 }
 
-/* [REQUISITO 1]: Função que, a partir da createProductItemElement(), adiciona 'id', 'title' e 'thumbnail' (imagem) de cada objeto presente no array retornado por getMeliComputers(), na <section class="items"></section> do index.html. */
-async function fillHtmlItemsSection() {
+/* [REQUISITO 1]: Função que, a partir da createProductItemElement(), adiciona 'id', 'title' e 'thumbnail' (imagem) de cada objeto presente no array retornado por getMeliProducts(), na <section class="items"></section> do index.html. */
+async function fillHtmlItemsSection(item = 'Smartphone') { // Caso nenhum argumento for passado para o parâmetro 'item', ele será preenchido com 'Smartphone'.
   const loadingElement = document.createElement('span'); // [REQUISITO 7]: Criando <span class="loading">Loading...</span>.
   loadingElement.className = 'loading';
   loadingElement.innerHTML = 'Loading...';
-  const bodyElement = document.getElementById('htmlBody');
-  bodyElement.insertAdjacentElement('afterbegin', loadingElement); // Inserindo <span class="loading">Loading...</span> como primeiro filho de <body></body>.
+  const headerElement = document.getElementById('htmlHeader');
+  headerElement.insertAdjacentElement('afterEnd', loadingElement); // Inserindo <span class="loading">Loading...</span> como primeiro filho de <body></body>.
 
-  const pcs = await getMeliComputers('https://api.mercadolibre.com/sites/MLB/search?q=computador'); // Utiliza-se o 'await' pois getMeliComputers() é assíncrona e preciso esperar seu retorno para seguir com as atividades síncronas abaixo.
+  const pcs = await getMeliProducts(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`); // Utiliza-se o 'await' pois getMeliProducts() é assíncrona e preciso esperar seu retorno para seguir com as atividades síncronas abaixo.
   document.querySelector('.loading').remove(); // Removendo <span class="loading">Loading...</span>, pois as informações da API já foram recebidas.
 
-  const compsContainer = document.querySelector('.items'); // Captando o elemento <section class="items"></section>.
+  const productsContainer = document.querySelector('.items'); // Captando o elemento <section class="items"></section>.
 
-  return pcs.forEach((computerObj) => { // Cria uma <section> que engloba 'id', 'title' e 'thumbnail' (imagem) de cada objeto presente em 'pcs', e a adiciona em 'compsContainer'.
-    compsContainer.appendChild(createProductItemElement(computerObj));
+  return pcs.forEach((productObj) => { // Cria uma <section> que engloba 'id', 'title' e 'thumbnail' (imagem) de cada objeto presente em 'pcs', e a adiciona em 'productsContainer'.
+    productsContainer.appendChild(createProductItemElement(productObj));
   });
 }
 
-async function getComputerInfo(id) { // [REQUISITO 2]: Função que faz uma requisição para a API do MELI visando captar as informações de um determinado produto, cuja identificação é igual à 'id'. Será utilizada dentro da addOnShopCart(). 
+/* [REQUISITO EXTRA]: Permite que o usuário realize pesquisas por produtos específicos. */
+function searchedProduct() {
+  const searchBtn = document.getElementById('searchBtn'); // Capturando o elemento <button id="searchBtn"></button>.
+  const displayedProducts = document.querySelector('.items'); // Capturando o elemento <section class="items"></section>.
+  const searchBox = document.getElementById('searchBox'); // Capturando o elemento <input type="text" placeholder="O que você procura?" id="searchBox">.
+
+  searchBtn.addEventListener('click', async () => { // Adicionando evento de clique no elemento <button id="searchBtn"></button>.
+    if(searchBox.value !== '') { // O clique só funcionará se o usuário inserir o nome de algum produto na caixa de pesquisa.
+      displayedProducts.innerHTML = ""; // Sempre que o botão for clicado, os produtos atualmente mostrados são apagados.
+      const productName = searchBox.value.split(' ').join('-'); // Ex: 'Macbook Pro M1' --> 'Macbook-Pro-M1'
+  
+      await fillHtmlItemsSection(productName); // A função que preenche a página é chamada e recebe o nome do produto pesquisado como argumento.
+      addOnShopCart();
+      getShopCartFromLocalStorage();
+      cleanShopCart();
+    }
+  });
+}
+
+async function getProductInfo(id) { // [REQUISITO 2]: Função que faz uma requisição para a API do MELI visando captar as informações de um determinado produto, cuja identificação é igual à 'id'. Será utilizada dentro da addOnShopCart(). 
   return fetch(`https://api.mercadolibre.com/items/${id}`)
     .then((response) => response.json());
 }
 
-function addOnShopCart() { // [REQUISITO 2]: Função que permite a adição de um determinado computador, ao carrinho de compras.
+function addOnShopCart() { // [REQUISITO 2]: Função que permite a adição de um determinado produto, ao carrinho de compras.
   const allAddOnCartBtnsArray = [...document.getElementsByClassName('item__add')]; // O getElementsByClassName(), que está captando todos os botões 'Adicionar ao carrinho!', retorna um HTML Collection. Para iterar ele, eu passei todo seu conteúdo para o Array 'allAddOnCartBtnsArray'.
   const shopCartContainer = getShopCartContainer(); // Captando o elemento <ol class="cart__items"></ol>.
   const cartTotalCont = cartValueContainer(); // Captando o <span id="mustBePayed"></span> e adicionando-o à constante cartTotalCont.
 
   return allAddOnCartBtnsArray.forEach((btn) => {
     btn.addEventListener('click', async (event) => { // Adicionando evento de clique para todos os botões 'Adicionar ao carrinho!'.
-      const clickedCompId = event.target.parentElement.firstElementChild.innerText; // Capturando a identificação (id) do computador quando seu respectivo botão 'Adicionar ao carrinho!', for clicado.
-      const clickedCompInfo = await getComputerInfo(clickedCompId); // Chamando a getComputerInfo() com o intuito de ter acesso às informações do computador adicionado ao carrinho.
-      const newElement = createCartItemElement(clickedCompInfo); // Criando <li></li>s referentes a cada computador adicionado ao carrinho.
+      const clickedProdId = event.target.parentElement.firstElementChild.innerText; // Capturando a identificação (id) do produto quando seu respectivo botão 'Adicionar ao carrinho!', for clicado.
+      const clickedProdInfo = await getProductInfo(clickedProdId); // Chamando a getProductInfo() com o intuito de ter acesso às informações do produto adicionado ao carrinho.
+      const newElement = createCartItemElement(clickedProdInfo); // Criando <li></li>s referentes a cada produto adicionado ao carrinho.
       shopCartContainer.appendChild(newElement); // Inserindo as <li></li>s criadas na linha acima, dentro de <ol class="cart__items"></ol>.
-      const r = Number((Number(cartTotalCont.innerHTML) + getComputerPrice(newElement)).toFixed(2)); // Captando o preço do computador recém-adicionado ao carrinho de compras e o adicionando ao innerHTML de <span id="mustBePayed"></span>.
+      const r = Number((Number(cartTotalCont.innerHTML) + getProductPrice(newElement)).toFixed(2)); // Captando o preço do produto recém-adicionado ao carrinho de compras e o adicionando ao innerHTML de <span id="mustBePayed"></span>.
       cartTotalCont.innerHTML = r; // Imputando o resultado da linha acima dentro de <span id="mustBePayed"></span>.
       saveShopCart(); // Chamando a função que salva os itens do carrinho no localStorage.
     });
@@ -150,4 +170,5 @@ window.onload = async () => { // A window.onload() é uma função como qualquer
   addOnShopCart();
   getShopCartFromLocalStorage();
   cleanShopCart();
-};
+  searchedProduct(); // Função que permite que o usuário procure por um determinado produto.
+}

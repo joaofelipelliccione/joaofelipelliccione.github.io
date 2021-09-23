a732856b33efbbedd6eedd62578dd12b8b58ccf1
João Felipe P <jf.pelliccione@gmail.com>
2021-09-23 08:35:14 -0300

    Organizando Diretórios do Portfólio - remoção.

--- projetos/Fundamentos-Desenvolvimento-Web/08_zoo-functions/src/getAnimalsOlderThan.js
+++
@@ -1,8 +0,0 @@
-const data = require('../data/zoo_data');
-
-function getAnimalsOlderThan(animal, age) {
-  const chosenSpecie = data.species.find((specie) => specie.name === animal);
-  return chosenSpecie.residents.every((resident) => resident.age >= age);
-}
-
-module.exports = getAnimalsOlderThan;

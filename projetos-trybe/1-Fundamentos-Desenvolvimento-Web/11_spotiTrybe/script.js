b936883fd60ecc2b467a9abe43addbbebeee63b6
João Felipe P <jf.pelliccione@gmail.com>
2021-09-27 17:13:12 -0300

    Recuperando gênero musical, junto à API.

--- projetos-trybe/1-Fundamentos-Desenvolvimento-Web/11_spotiTrybe/script.js
+++ projetos-trybe/1-Fundamentos-Desenvolvimento-Web/11_spotiTrybe/script.js
@@ -1,8 +1,11 @@
 // https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow --> Como realizar a requisição para API.
 const CLIENT_ID = '166de45747534183a240ddf829ead0f9';
 const CLIENT_SECRET = '5627899d6f114ad9ab0911a1fda15f28';
+const BASE_URL = 'https://api.spotify.com/v1'
 
 let token;
+
+// API SPOTIFY
 
 async function getToken() {
   const idAndSecret = `${CLIENT_ID}:${CLIENT_SECRET}`;
@@ -24,6 +27,49 @@ async function getToken() {
   token = data.access_token;
 }
 
-window.onload = function() {
-  getToken();
+async function getGenres() {
+  const headers = new Headers({
+    'Authorization': `Bearer ${token}`
+  })
+
+  response = await fetch(`${BASE_URL}/browse/categories?country=BR&locale=pt-BR`, {
+    headers
+  });
+
+  const data = await response.json();
+
+  renderGenres(data.categories.items)
+}
+
+
+// RENDERS
+
+function renderGenres(genres) {
+  const genresCards = document.querySelector('.genres-cards')
+
+  genres.forEach((genre) => {
+    const section = document.createElement('section');
+    section.className = 'genre';
+
+    const image = document.createElement('img');
+    image.src = genre.icons[0].url;
+
+    const paragraph = document.createElement('p');
+    paragraph.innerText = genre.name;
+
+    section.appendChild(image);
+    section.appendChild(paragraph);
+
+    genresCards.appendChild(section);
+  })
+}
+
+window.onload = async () => {
+  try {
+    await getToken();
+
+    getGenres();
+  }catch (error) {
+    console.log(error)
+  }
 }

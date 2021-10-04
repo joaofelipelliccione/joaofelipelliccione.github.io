41d3355f95ec1807aa5746ab7821fe097eccb810
João Felipe P <jf.pelliccione@gmail.com>
2021-10-04 19:50:11 -0300

    Criação do botão de filtro "Fire".

--- projetos-trybe/2-Frontend/10_pokedex-aula/my-pokedex/src/App.js
+++ projetos-trybe/2-Frontend/10_pokedex-aula/my-pokedex/src/App.js
@@ -8,14 +8,31 @@ class App extends React.Component {
     super();
 
     this.state = {
+      pokemons: pokemonsData,
       numOfClicksNextPokBtn: 0,
+      fireFilter: 'disable',
     }
 
+    this.fireBtnFunc = this.fireBtnFunc.bind(this);
     this.nextPokemonBtnFunc = this.nextPokemonBtnFunc.bind(this);
   }
 
+  fireBtnFunc() {
+    if(this.state.fireFilter === 'disable') {
+      this.setState(() => ({
+        fireFilter: 'enable',
+        pokemons: pokemonsData.filter((pokemons) => pokemons.type === 'Fire'),
+      }));
+    } else {
+      this.setState(() => ({
+        fireFilter: 'disable',
+        pokemons: pokemonsData,
+      }));
+    }
+  }
+
   nextPokemonBtnFunc() {
-    if(this.state.numOfClicksNextPokBtn === pokemonsData.length - 1) {
+    if(this.state.numOfClicksNextPokBtn === this.state.pokemons.length - 1) {
       this.setState(() => ({
         numOfClicksNextPokBtn: -1
       }));
@@ -25,8 +42,6 @@ class App extends React.Component {
     }));
   }
 
-  
-  
   render() {
     return(
       <main>
@@ -34,9 +49,10 @@ class App extends React.Component {
           <h1>Pokédex</h1>
         </header>
         <article id='pokemonsContainer'>
-          <Pokemon pokemonData={pokemonsData} pokemonDataIndex={this.state.numOfClicksNextPokBtn}></Pokemon>
+          <Pokemon pokemonData={this.state.pokemons} pokemonDataIndex={this.state.numOfClicksNextPokBtn}></Pokemon>
           <section className='btnsContainer'>
             <button onClick={this.nextPokemonBtnFunc} id='nextPokemonBtn'>Próximo Pokémon</button>
+            <button onClick={this.fireBtnFunc} id='fireBtn'>{this.state.fireFilter}</button>
           </section>
         </article>
       </main>
